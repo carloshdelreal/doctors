4ea76650c6ca6e620e5d3c89595a9448f7991728
Carlos Del Real <carloshdelreal@gmail.com>
2020-01-06 22:48:43 -0500

    setting up webpacker styling and bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,22 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from '../components/App'
+import '../stylesheets/main'
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <App />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
