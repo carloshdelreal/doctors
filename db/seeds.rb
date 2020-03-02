d3cc8070a9b596dc5c003b6858ccfda39a64ae25
Carlos Del Real <carloshdelreal@gmail.com>
2020-03-02 09:41:31 -0500

    improve seeds with more appointments and data

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,72 +45,12 @@ s = Specialization.all
                   specialization_id: s.sample.id )
 end
 
-doctors = Doctor.all
-users = User.all
-confirmed_array = [true, false]
-doctors.each do |doctor|
-  u = users.sample
-  d = Date.today + 1.day
-  t = Time.new(d.year, d.month, d.day, 8)
-  doctor.appointments.create!(
-    user_id: u.id,
-    date: d,
-    time: t,
-    duration: 1,
-    confirmed: confirmed_array.sample,
-  )
 
-  u = users.sample
-  t = Time.new(d.year, d.month, d.day, 9)
-  doctor.appointments.create!(
-    user_id: u.id,
-    date: d,
-    time: t,
-    duration: 1,
-    confirmed: confirmed_array.sample,
-  )
-
-  u = users.sample
-  t = Time.new(d.year, d.month, d.day, 11)
-  doctor.appointments.create!(
-    user_id: u.id,
-    date: d,
-    time: t,
-    duration: 1,
-    confirmed: confirmed_array.sample,
-  )
-
-  u = users.sample
-  d = Date.today + 1.day
-  t = Time.new(d.year, d.month, d.day, 12)
-  doctor.appointments.create!(
-    user_id: u.id,
-    date: d,
-    time: t,
-    duration: 1,
-    confirmed: confirmed_array.sample,
-  )
+200.times do |x|
+  Atend.create(date: Time.now() + x.day)
 end
 
-Atend.create(date: Time.now() + 1.day)
-Atend.create(date: Time.now() + 2.day)
-Atend.create(date: Time.now() + 3.day)
-Atend.create(date: Time.now() + 4.day)
-Atend.create(date: Time.now() + 5.day)
-Atend.create(date: Time.now() + 6.day)
-Atend.create(date: Time.now() + 7.day)
-Atend.create(date: Time.now() + 8.day)
-
-def createBookings(d, a)
-  Booking.create(doctor: d, atend: a, label: "8:00 AM", hour: 8, minutes: 00, booked: false )
-  Booking.create(doctor: d, atend: a, label: "8:30 AM", hour: 8, minutes: 30, booked: false )
-  Booking.create(doctor: d, atend: a, label: "9:00 AM", hour: 9, minutes: 00, booked: false )
-  Booking.create(doctor: d, atend: a, label: "9:30 AM", hour: 9, minutes: 30, booked: false )
-  Booking.create(doctor: d, atend: a, label: "10:00 AM", hour: 10, minutes: 00, booked: false )
-  Booking.create(doctor: d, atend: a, label: "10:30 AM", hour: 10, minutes: 30, booked: false )
-  Booking.create(doctor: d, atend: a, label: "11:00 AM", hour: 11, minutes: 00, booked: false )
-  Booking.create(doctor: d, atend: a, label: "11:30 AM", hour: 11, minutes: 30, booked: false )
-  Booking.create(doctor: d, atend: a, label: "12:00 AM", hour: 12, minutes: 00, booked: false )
+def createAfternoonBookings(d, a)
   Booking.create(doctor: d, atend: a, label: "2:00 PM", hour: 14, minutes: 00, booked: false )
   Booking.create(doctor: d, atend: a, label: "2:30 PM", hour: 14, minutes: 30, booked: false )
   Booking.create(doctor: d, atend: a, label: "3:00 PM", hour: 15, minutes: 00, booked: false )
@@ -131,31 +71,24 @@ def createMorningBookings(d, a)
   Booking.create(doctor: d, atend: a, label: "12:00 AM", hour: 12, minutes: 00, booked: false )
 end
 
-a1 = Atend.first
-a2 = Atend.second
-a3 = Atend.third
-a4 = Atend.all[4]
-a5 = Atend.all[5]
-a6 = Atend.all[6]
-
 Doctor.all.each do | d |
-  createBookings(d, a1)
-end
-
-
-Doctor.all.shuffle[0..35].each do | d |
-  createBookings(d, a2)
-  createBookings(d, a3)
+  Atend.all.each do | a |
+    createAfternoonBookings(d, a)
+  end
 end
 
 Doctor.all.shuffle[0..35].each do | d |
-  createMorningBookings(d, a4)
-  createMorningBookings(d, a5)
+  Atend.all.each do | a |
+    createMorningBookings(d, a)
+  end
 end
 
-Doctor.all.shuffle[0..35].each do | d |
-  createMorningBookings(d, a6)
+User.all.each do | user |
+  Doctor.all.shuffle[0..5] do | doctor |
+    booking = Booking.where(doctor_id: doctor.id booked: false user_id: nill).sample
+    booking.user_id = user.id
+    booking.booked = true
+    booking.save
+  end
 end
 
-
-  
