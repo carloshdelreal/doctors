1ba262791d4bbb286d758b74830953eed00ed9cc
Carlos Del Real <carloshdelreal@gmail.com>
2020-03-03 09:58:44 -0500

    add color checks to the past and upcoming appointments

--- app/javascript/components/admin/booking.jsx
+++ app/javascript/components/admin/booking.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React from 'react';
 import sortBookingByDatetime from '../../scripts/sorting';
+import grayCheck from '../../images/graycheck.png';
+import greenCheck from '../../images/greencheck.png';
 
 class BookingComponent extends React.Component {
   constructor(props) {
@@ -12,7 +14,20 @@ class BookingComponent extends React.Component {
 
   async componentDidMount() {
     const res = await axios.get('/api/v1/user/booking');
-    this.setState({ listBooking: sortBookingByDatetime(res.data.booking) });
+    const booking = sortBookingByDatetime(res.data.booking);
+
+    booking.forEach(item => {
+      item.datetimeObject = new Date(
+        item.datetime.slice(0, 4),
+        parseInt(item.datetime.slice(5, 7), 10) - 1,
+        item.datetime.slice(8, 10),
+        item.datetime.slice(11, 13),
+        item.datetime.slice(14, 16),
+        0,
+      );
+    });
+
+    this.setState({ listBooking: booking });
   }
 
   render() {
@@ -32,19 +47,17 @@ class BookingComponent extends React.Component {
       <div>
         { listBooking.map(item => (
           <div key={item.booking_id} className="container">
-            <div className="row">
-              <div className="booking col-12">
+            <div className="row bookingRow">
+              <div className="booking col-12 pr-4">
                 <h5>
-                  { `${new Date(
-                    item.datetime.slice(0, 4),
-                    parseInt(item.datetime.slice(5, 7), 10) - 1,
-                    item.datetime.slice(8, 10),
-                    item.datetime.slice(11, 13),
-                    item.datetime.slice(14, 16),
-                    0,
-                  )}, at ${item.label} ` }
+                  { `${item.datetimeObject}` }
                 </h5>
                 <p>{ `With Dr. ${item.doctor.fullname}, ${item.specialty}, at ${item.doctor.location}` }</p>
+              </div>
+              <div className="bookingCheck">
+                { item.datetimeObject <= new Date()
+                  ? <img src={grayCheck} alt="gray check" />
+                  : <img src={greenCheck} alt="green check" /> }
               </div>
             </div>
           </div>
