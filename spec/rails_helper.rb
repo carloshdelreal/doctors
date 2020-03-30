5eb426aef496a394c290ebaa08ba9db4d79f93af
Carlos Del Real <carloshdelreal@gmail.com>
2020-03-30 18:11:23 -0500

    update rails helper to be able to use devise methods

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,7 @@ end
 require 'rspec/rails'
 require 'capybara/rails'
 RSpec.configure do |config|
+  config.include Rails.application.routes.url_helpers
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
   config.include Warden::Test::Helpers
